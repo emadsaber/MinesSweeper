a1d2d0fc1284b0083930bdac7c3bf80e8b539c81
Emad Saber Hamed <omda.sharkawy@gmail.com>
2018-04-03 22:54:34 +0200

    calculating surrounding, disable zero cells, hanled game over and new game
    - calculating surrounding
    - disable zero cells
    - hanled game over and new game

--- js/MainApp/Controllers/GameController.js
+++ js/MainApp/Controllers/GameController.js
@@ -3,12 +3,13 @@ var gameController = gameApp.controller('gameController', function($scope){
     $scope.settings = {
         rows: 10,
         columns: 12,
-        difficulty: 24,
+        difficulty: 40,
     };
 
     $scope.rows = $scope.settings.rows;
     $scope.columns = $scope.settings.columns;
     $scope.mines = [];
+    $scope.gameOverFlag = false;
 
     pageLoad();
 
@@ -19,17 +20,27 @@ var gameController = gameApp.controller('gameController', function($scope){
         return arr;
     }
     $scope.cellClicked = function(cellName){
+        if($scope.gameOverFlag) return;
+
         var rowIndex = cellName.row;
         var colIndex = cellName.col;
         
-        var indexes = jQuery.map($scope.mines, function(val, idx){
-            if(val.x == colIndex && val.y == rowIndex) return idx;
-        });
-
-        if(indexes != undefined && indexes.length != 0){
-            showBomb($scope.mines[indexes[0]]);
+        if(isMine(rowIndex, colIndex)){
+            showAllMines();
+            showGameOver();
+            return;
+        }else{
+            var minesCount = getSurroundingMinesCount(colIndex, rowIndex);
+            if(minesCount != 0)
+                showNumber(rowIndex, colIndex, minesCount);
+            else 
+                disableCellAndSurroundingCells(rowIndex, colIndex);
         }
 
+    }
+    $scope.newGame = function(){
+        $scope.gameOverFlag = false;
+        location.reload(true);
     }
     //private functions
     function getMinesLocations(){
@@ -47,17 +58,75 @@ var gameController = gameApp.controller('gameController', function($scope){
             y : Math.floor(Math.random() * $scope.settings.rows),
         };
     }
+    function isMine(rowIndex, colIndex){
+        var indexes = jQuery.map($scope.mines, function(val, idx){
+            if(val.x == colIndex && val.y == rowIndex) return idx;
+        });
 
+        if(indexes != undefined && indexes.length != 0) return true;
+        return false;
+    }
     function pageLoad(){
         $scope.mines = getMinesLocations();
-        
-        console.log($scope.mines);
     }
-
-    function showBomb(mineLocation){
-        var row = mineLocation.y;
-        var col = mineLocation.x;
+    function showAllMines(){
+        $scope.mines.forEach(function(mine){
+            showMine(mine.y, mine.x);
+        });
+    }
+    function showMine(row, col){
         var cellName = row + "_" + col;
         $("[name='" + cellName + "']").css("background-color", "black");
+    }
+    function getSurroundingMinesCount(x , y){
+        var x_max = $scope.settings.rows - 1;
+        var y_max = $scope.settings.columns - 1;
+
+        //decide mine surroundings
+        // c0  c1  c2
+        // c7      c3
+        // c6  c5  c4
+        var cells = getSurroundingCells(x, y);
+        var minesCount = 0;
+        
+        cells.forEach(function(cell){
+            if(isMine(cell.y, cell.x)) minesCount ++;
+        });
+
+        return minesCount;
+    }
+    function getSurroundingCells(x, y){
+        return [{x: x - 1, y: y - 1},
+                {x: x    , y: y - 1},
+                {x: x + 1, y: y - 1},
+                {x: x + 1, y: y    },
+                {x: x + 1, y: y + 1},
+                {x: x    , y: y + 1},
+                {x: x - 1, y: y + 1},
+                {x: x - 1, y: y    }];
+    }
+    function showNumber(row, col, number){
+        var cellName = row + "_" + col;
+        $("[name='" + cellName + "']").val(number);
+    }
+    function showGameOver(){
+        $(".gameOver").show(500);
+    }
+    function disableCellAndSurroundingCells(row, col){
+        disableCell(row, col);
+        
+        var cells = getSurroundingCells(col, row);
+        cells.forEach(function(cell){
+            var minesCount = getSurroundingMinesCount(cell.x, cell.y);
+            if(minesCount == 0){
+                disableCell(cell.y, cell.x);
+            }else{
+                showNumber(cell.y, cell.x, minesCount);
+            }
+        });
+    }
+    function disableCell(row, col){
+        var cellName = row + "_" + col;
+        $("[name='" + cellName + "']").addClass("disabled");
     }
 })
